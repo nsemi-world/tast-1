4ef7110a222c05b63519c2cc526a73f7690e4ffc
Alexandre Caldeira <zuacaldeira@gmail.com>
2018-10-19 23:12:55 +0200

    Change PDO initialization with new database info. 6

--- scripts/allByYear.php
+++ scripts/allByYear.php
@@ -3,7 +3,7 @@
     include '../lib/classes/autoloader.php';    
     header('Content-type:application/json; charset=utf-8');
 
-    $helper = new db\VoyagesDatabaseHelper();
+    $helper = new \db\VoyagesDatabaseHelper();
     $result   = $helper->findSummariesByYear();
     echo json_encode($result);
 
