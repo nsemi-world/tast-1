88a744949b5053e26179f57981887df2cc6f4236
Alexandre Caldeira <zuacaldeira@gmail.com>
2018-10-20 14:40:17 +0200

    Remove use of external classes in getCitation.php

--- php/getCitation.php
+++ php/getCitation.php
@@ -1,14 +1,24 @@
 <?php
 
 //include 'autoloader.php';;
+$host = 'us-cdbr-iron-east-01.cleardb.net';
+$username = 'b0bd1223927bc6';
+$password = '707fe6cf';
+$dbname = 'heroku_1bca0db043051c1';
+$pdo = new PDO("mysql:host=".$host."; dbname=".$dbname.'; charset=utf8', $username, $password);
 
-$helper = new \db\VoyagesDatabaseHelper();
-$citation = $helper->findCitation();
+$citation = findCitation($pdo);
 
 header('Content-type:application/json;charset=utf-8');
 echo json_encode($citation);
 
 
 
+function findCitation($pdo) {
+    $query = "SELECT * FROM citations";
+    $erg = $pdo->query($query);
+    $result = $erg->fetchAll(PDO::FETCH_OBJ);
+    return $result[array_rand($result)];
+}
 
 ?>
