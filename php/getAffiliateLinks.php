45f956c176239fc6b81af132b41188e570c23ee2
Alexandre Caldeira <zuacaldeira@gmail.com>
2018-10-20 14:35:13 +0200

    Remove use of external classes in getAffiliatedLInks.php

--- php/getAffiliateLinks.php
+++ php/getAffiliateLinks.php
@@ -13,10 +13,24 @@ if(isset($_GET['value'])) {
     $value = $_GET['value'];
 }
 
-$helper = new VoyagesDatabaseHelper();
-$links = $helper->findAffiliateLinks($keyword, $value);
+$host = 'us-cdbr-iron-east-01.cleardb.net';
+$username = 'b0bd1223927bc6';
+$password = '707fe6cf';
+$dbname = 'heroku_1bca0db043051c1';
+$pdo = new PDO("mysql:host=".$host."; dbname=".$dbname.'; charset=utf8', $username, $password);
+
+$links = findAffiliateLinks($pdo, $keyword, $value);
 
 header('Content-type:application/json;charset=utf-8');
 echo json_encode($links);
+
+
+function findAffiliateLinks($pdo, $keyword, $value) {
+    $query = "SELECT * FROM affiliate WHERE $keyword LIKE \"%$value%\"";
+    $erg = $pdo->query($query);
+    $result =  $erg->fetchAll(PDO::FETCH_OBJ);
+    return $result;
+}
+    
 
 ?>
