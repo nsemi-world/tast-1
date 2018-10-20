29b5894078c0f9036e2e8616d1f4f262a474be6f
Alexandre Caldeira <zuacaldeira@gmail.com>
2018-10-20 13:44:04 +0200

    Set autoloader namespace to introduce app\php namespace.

--- php/autoloader.php
+++ php/autoloader.php
@@ -1,10 +1,11 @@
 <?php
+namespace app\php;
 
-    spl_autoload_extensions('.php');
+spl_autoload_extensions('.php');
 
-    spl_autoload_register(function($name) {
-        if($name != 'db\PDO') {
-            //echo '<h2>Autoloader: '.$name.'</h2>';
-            spl_autoload($name);
-        }
-    });
+spl_autoload_register(function($name) {
+    if($name != 'db\PDO') {
+        //echo '<h2>Autoloader: '.$name.'</h2>';
+        spl_autoload($name);
+    }
+});
