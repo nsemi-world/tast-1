15a1da405de58f83514fae818f61324f2fdc0c5c
Alexandre Caldeira <zuacaldeira@gmail.com>
2018-10-20 14:53:19 +0200

    Remove use of external classes in getCaptains.php

--- php/getCaptains.php
+++ php/getCaptains.php
@@ -1,9 +1,12 @@
 <?php
 
-//include 'autoloader.php';;
+$host = 'us-cdbr-iron-east-01.cleardb.net';
+$username = 'b0bd1223927bc6';
+$password = '707fe6cf';
+$dbname = 'heroku_1bca0db043051c1';
+$pdo = new PDO("mysql:host=".$host."; dbname=".$dbname.'; charset=utf8', $username, $password);
 
-$helper = new VoyagesDatabaseHelper();
-$captains = $helper->findCaptainsSummaries();
+$captains = findCaptainsSummaries($pdo);
 
 $result = [];
 
@@ -21,5 +24,27 @@ foreach($captains as $captain) {
 
 header('Content-type:application/json;charset=utf-8');
 echo json_encode($result);
+
+function findCaptainsSummaries($pdo) {
+    $query = 
+          "SELECT \n"
+        . "	   captaina as name, \n"
+        . "    COUNT(shipname) as ships, \n"
+        . "    COUNT(voyageid) as voyages, \n"
+        . "    SUM(crew) as crew, \n"
+        . "    SUM(slaximp) as embarked,\n"
+        . "    SUM(slamimp) as disembarked,\n"
+        . "    SUM(slaximp)-SUM(slamimp) as died\n"
+        . "\n"
+        . "FROM voyages\n"
+        . "WHERE captaina != ''\n"
+        . "GROUP BY captaina";
+    //var_dump($query);
+    $erg = $pdo->query($query);
+    $result =  $erg->fetchAll(PDO::FETCH_OBJ);
+    return $result;
+}
+    
+
 
 ?>
