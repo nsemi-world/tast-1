a1210dc5cecc2dc116c38cc2e395dbc8bdfb6c30
Alexandre Caldeira <zuacaldeira@gmail.com>
2018-10-20 14:48:27 +0200

    Remove use of external classes in getCountriesSeries.php

--- php/getCountriesSeries.php
+++ php/getCountriesSeries.php
@@ -1,17 +1,21 @@
 <?php
 //include 'autoloader.php';;
+$host = 'us-cdbr-iron-east-01.cleardb.net';
+$username = 'b0bd1223927bc6';
+$password = '707fe6cf';
+$dbname = 'heroku_1bca0db043051c1';
+$pdo = new PDO("mysql:host=".$host."; dbname=".$dbname.'; charset=utf8', $username, $password);
 
 $year = 0;
 if(isset($_GET['year'])) {
     $year = $_GET['year'];
 }
 
-$helper = new VoyagesDatabaseHelper();
 if($year == 0) {
-    $result = $helper->findCountriesSummaries();
+    $result = findCountriesSummaries($pdo);
 }
 else {
-    $result = $helper->findCountriesSummariesForYear($year);
+    $result = findCountriesSummariesForYear($pdo, $year);
 }
 
 $series = [];
@@ -29,5 +33,93 @@ foreach($result as $key => $value) {
 header('Content-type:application/json;charset=utf-8');
 
 echo json_encode($series);
+
+
+function findCountriesSummaries($pdo) {
+    $sql = "SELECT \n"
+        . "	label as name, \n"
+        . "    value as id, \n"
+        . "    iso3 as countryCode, \n"
+        . "    ships,\n"
+        . "    SUM(embarked) as total_embarked,\n"
+        . "    SUM(disembarked) as total_disembarked,\n"
+        . "    SUM(embarked)-SUM(disembarked) as total_died\n"
+        . "	    \n"
+        . "FROM\n"
+        . "(\n"
+        . "    SELECT \n"
+        . "        n.label,\n"
+        . "        n.value,\n"
+        . "        n.iso3,\n"
+        . "    	COUNT(DISTINCT shipname) as ships,\n"
+        . "        SUM(v.slaximp) as embarked,\n"
+        . "    	SUM(v.slamimp) as disembarked\n"
+        . "    FROM `voyages` as v\n"
+        . "    JOIN `natinimp` as n ON n.value=v.natinimp\n"
+        . "    GROUP BY n.iso3\n"
+        . "\n"
+        . "    UNION\n"
+        . "\n"
+        . "    SELECT \n"
+        . "        n.label,\n"
+        . "        n.value,\n"
+        . "        n.iso3,\n"
+        . "    	COUNT(DISTINCT shipname) as ships,\n"
+        . "        SUM(v.slaximp) as embarked,\n"
+        . "    	SUM(v.slamimp) as disembarked    \n"
+        . "    FROM `voyages` as v\n"
+        . "    JOIN `national` as n ON n.value=v.national\n"
+        . "    GROUP BY n.iso3\n"
+        . ") AS temp \n"
+        . "GROUP BY iso3\n"
+        . "ORDER BY total_embarked DESC";
+    $erg = $pdo->query($sql);
+    return $erg->fetchAll(PDO::FETCH_OBJ);
+}
+
+function findCountriesSummariesForYear($pdo, $year) {
+    $sql = "SELECT \n"
+        . "	label as name, \n"
+        . "    value as id, \n"
+        . "    iso3 as countryCode, \n"
+        . "    ships,\n"
+        . "    SUM(embarked) as total_embarked,\n"
+        . "    SUM(disembarked) as total_disembarked,\n"
+        . "    SUM(embarked)-SUM(disembarked) as total_died\n"
+        . "	    \n"
+        . "FROM\n"
+        . "(\n"
+        . "    SELECT \n"
+        . "        n.label,\n"
+        . "        n.value,\n"
+        . "        n.iso3,\n"
+        . "    	COUNT(DISTINCT shipname) as ships,\n"
+        . "        SUM(v.slaximp) as embarked,\n"
+        . "    	SUM(v.slamimp) as disembarked\n"
+        . "    FROM `voyages` as v\n"
+        . "    JOIN `natinimp` as n ON n.value=v.natinimp\n"
+        . "    WHERE v.yeardep=$year\n"
+        . "    GROUP BY n.iso3\n"
+        . "\n"
+        . "    UNION\n"
+        . "\n"
+        . "    SELECT \n"
+        . "        n.label,\n"
+        . "        n.value,\n"
+        . "        n.iso3,\n"
+        . "    	COUNT(DISTINCT shipname) as ships,\n"
+        . "        SUM(v.slaximp) as embarked,\n"
+        . "    	SUM(v.slamimp) as disembarked    \n"
+        . "    FROM `voyages` as v\n"
+        . "    JOIN `national` as n ON n.value=v.national\n"
+        . "    WHERE v.yeardep=$year\n"
+        . "    GROUP BY n.iso3\n"
+        . ") AS temp \n"
+        . "GROUP BY iso3\n"
+        . "ORDER BY total_embarked DESC";
+    $erg = $pdo->query($sql);
+    return $erg->fetchAll(PDO::FETCH_OBJ);
+}
+
 
 ?>
