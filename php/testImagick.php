1489a631590afe3db32095f44218e01692ed298a
Alexandre Caldeira <zuacaldeira@gmail.com>
2018-11-12 18:28:36 +0100

    Integrates imagick during article creation.

--- php/testImagick.php
+++ php/testImagick.php
@@ -3,6 +3,11 @@
 require_once('./utils.php');
 require '../vendor/autoload.php';
 
+use Aws\Credentials\CredentialProvider;
+use Aws\S3\S3Client;
+
+$AWS_IMG = 'https://s3.eu-central-1.amazonaws.com/tastxplorer/img/';
+
 /*
 $image = new Imagick();
 $image->newImage(1, 1, new ImagickPixel('#ffffff'));
@@ -16,7 +21,9 @@ $relativename = "./test/images/myimage.jpg";
 $filename = getFilename($relativename);
 
 
-compress($filename, 50, true);
+$im = compress($filename, 50, true);
+saveImageToAws("test", $im);
+
 //resize($filename, 400, 300);
 
 
@@ -36,18 +43,19 @@ function compress($filename, $cquality, $minify) {
     $im->setImageCompression(Imagick::COMPRESSION_JPEG);
     $im->setImageCompressionQuality($cquality);
 
-    // Write the image back
+    /* Write the image back
     $filename_out = "C:/xampp/htdocs/websites/tast/php/test/images/myimage$cquality.jpg";
     $im->writeImages($filename_out, true);
-
+    */
 
     if($minify) {
         $im->minifyImage();
-        // Write the image back
+        /* Write the image back
         $filename_out_min = "C:/xampp/htdocs/websites/tast/php/test/images/myimage25-min.jpg";
         $im->writeImages($filename_out, true);
+        */
     }
-
+    return $im;
 }
 
 function resize($filename, $width, $height) {
@@ -56,7 +64,51 @@ function resize($filename, $width, $height) {
     
     $filename_out = "C:/xampp/htdocs/websites/tast/php/test/images/myimage$width-$height.jpg";
     $im->writeImages($filename_out, true);
-    
+    return $im;
 }
+
+function saveImageToAWS($filename, $image) {
+    $provider = CredentialProvider::env();
+    
+    $s3 = new S3Client([
+        'version' => 'latest',
+        'region'  => 'eu-central-1',
+        'provider' => $provider
+    ]);
+    
+    try {
+        
+        $result = $s3->putObject([
+            'Bucket' => 'tastxplorer',
+            'Key'    => 'atricles-img/min/' . $filename,
+            //'ContentEncoding' => 'base64',
+            'ContentType' => 'image/' . $image->getImageFormat(),
+            'Body'   => $image->getImageBlob(),
+            'ACL'   => 'public-read'
+        ]);
+
+        // Print the URL to the object.
+        $b =  $result['ObjectURL'] . PHP_EOL;
+        echo $b;
+        return $b;
+    } catch (S3Exception $e) {
+        echo 'S3 Exception' . $e->getMessage() . PHP_EOL;
+    }        
+}
+
+function optimizeImage($image_decoded) {
+    $im = new Imagick();
+    $im->readImageBlob($image_decoded);
+    // Optimize the image layers
+    $im->optimizeImageLayers();
+
+    // Compression and quality
+    $im->setImageCompression(Imagick::COMPRESSION_JPEG);
+    $im->setImageCompressionQuality($cquality);
+
+    $im->minifyImage();
+    return $im;
+}
+
 
 ?>
