3dfc77f6b17fd2c074d8c79b9ccde7906ef3af82
Alexandre Caldeira <zuacaldeira@gmail.com>
2018-11-13 02:35:46 +0100

    Optimize images, 50% compression, do not minify

--- php/createArticle.php
+++ php/createArticle.php
@@ -44,7 +44,7 @@ function saveImageToAWS($filename, $image) {
     try {
         // Decode the image
         $image_decoded = base64_decode(getImageData($image));
-        $im = optimizeImage($image_decoded, 50, true);
+        $im = optimizeImage($image_decoded, 50, false);
         
         $result = $s3->putObject([
             'Bucket' => 'tastxplorer',
