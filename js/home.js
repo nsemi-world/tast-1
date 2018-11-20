69abb23804f416817c34a18972304db52a49d682
Alexandre Caldeira <zuacaldeira@gmail.com>
2018-11-20 09:35:36 +0100

    Style top quizz

--- js/home.js
+++ js/home.js
@@ -1,48 +1,45 @@
+var quizzData = null;
+var quizzStarted = false;
+
 $(document).ready(function () {
     activate($('#toggle_home'));
-    centerHome();
+    centerHomeTitle();
     loadSectionImage('#home .frontpage', 'home.jpg');
     configureSubsections();
 
     $(window).on('resize', function () {
-        centerHome();
+        centerHomeTitle();
         debounce('#home .frontpage', 'home.jpg');
     });
 
     $('#top3-controls #number-of-tops').on('change', function (event) {
-        $('#top3-options #top-value').text($(this).val());
-        $('#ncorrect').text('0');
-        $('#ntries').text('0');
+        getUserScoreCorrectAnswers().text('0');
+        getUserScoreTotalAttempts().text('0');
         updateQuizz();
     });
-    
-    $('#top3-options label').on('click', function (event) {
+
+    $('#top3-options li').on('click', function (event) {
         event.preventDefault();
         activateOption($(this));
         updateQuizz();
     });
-    $('#top3-criteria label').on('click', function (event) {
+    $('#top3-criteria li').on('click', function (event) {
         event.preventDefault();
         activateCriteria($(this));
         updateQuizz();
     });
 
+    getMenuCollapseButton().on('click', function (event) {
+        event.preventDefault();
+        getMenuOptions().slideToggle(1000);
+        getMenuCriteria().slideToggle(1000);
+    });
+
     updateQuizz();
 });
 
-
-function activateOption($option) {
-    $('#top3-options label.active').removeClass('active');
-    $option.addClass('active');
-}
-
-function activateCriteria($criteria) {
-    $('#top3-criteria label.active').removeClass('active');
-    $criteria.addClass('active');
-    sortQuizzByCriteriaDesc($(this).val());
-}
-
-function centerHome() {
+// Positioning
+function centerHomeTitle() {
     $('#home .title-wrapper .title').position({
         my: 'center',
         at: 'center',
@@ -50,95 +47,267 @@ function centerHome() {
     });
 }
 
-function updateQuizz() {
-    disableQuizz();    
-    var question = getQuizzQuestion();
-    var answers = getQuizzAnswers();
-    $('#top3-quizz-title').html(question);
-    $('#user-answers').html(answers);
-    $('#user-results #ntotal').text($('#number-of-tops').val());
 
+/** 
+ * ---------------------------------------
+ * Accessors to Quizz dom elements
+ * ------------------------------------------ 
+ */
+function getQuizz() {
+    return $('#top3-quizz');
+}
+
+function getQuizzTitle() {
+    return $('#top3-quizz-title');
+}
+
+function getQuizzMenu() {
+    return $('#top3-quizz #top3-menu');
+}
+
+function getMenuCollapseButton() {
+    return $('#top3-controls #collapse-menu-button');
+}
+
+function getUserScore() {
+    return $('#user-score');
+}
+
+function getUserBadge() {
+    return $('#score-badge');
+}
+
+function getUserScoreCorrectAnswers() {
+    return $('#score-badge .ncorrect');
+}
+
+function getUserScoreTotalAnswers() {
+    return $('#score-badge .ntotal');
+}
+
+function getUserScoreTotalAttempts() {
+    return $('#score-badge .ntries');
+}
+
+function getUserScorePercentage() {
+    return $('#score-badge .percentage');
+}
+
+function getMenuControls() {
+    return $('#top3-menu #top3-controls ');
+}
+
+function getMenuTopValue() {
+    return $('#top3-options #top-value');
+}
+
+function getNumberOfTopsSelector() {
+    return $('#top3-controls #number-of-tops option:selected');
+}
+
+function getMenuOptions() {
+    return $('#top3-menu #top3-options ');
+}
+
+function getMenuCriteria() {
+    return $('#top3-menu #top3-criteria ');
+}
+
+function getCriteriaEmbarked() {
+    return $('#top3-criteria #criteria-embarked');
+}
+
+function getCriteriaDisembarked() {
+    return $('#top3-criteria #criteria-disembarked');
+}
+
+function getCriteriaDied() {
+    return $('#top3-criteria #criteria-died');
+}
+
+function getUserAnswers() {
+    return $('#user-answers');
+}
+
+function getPossibleAnswers() {
+    return $('#possible-answers');
+}
+
+function getActiveOption() {
+    return $('#top3-options li.active');
+}
+
+function getActiveCriteria() {
+    return $('#top3-criteria li.active');
+}
+
+
+/** 
+ * ---------------------------------------
+ * Activate Options and Criteria
+ * ------------------------------------------ 
+ */
+function activateOption($option) {
+    getActiveOption().removeClass('active');
+    $option.addClass('active');
+}
+
+function activateCriteria($criteria) {
+    getActiveCriteria().removeClass('active');
+    $criteria.addClass('active');
+}
+
+
+/**
+ * Update quizz consists by restart the quizz configuration according 
+ * to user input. This consists of the following steps:
+ *  1 - Disable the quizz to disable more user inputs
+ *  2 - Update the quizz question, and prepare user answers area
+ *  3 - Call the server to fetch the quizz data
+ */
+function updateQuizz() {
+    //disableQuizz(); // 1 - Disable quizz   
+    updateQuizzData(); // 2 - Update quizz data
+    loadAnswersFromServer(); // 3 - Call server
+}
+
+function enableQuizzMenu() {
+    getQuizzMenu().slideDown(1000);
+}
+
+function disableQuizzMenu() {
+    getQuizzMenu().slideUp(1000);
+}
+
+function updateQuizzData() {
+    getQuizzTitle().html(createQuizzQuestion());
+    getUserScoreTotalAnswers().text(getNumberOfTopsSelector().val());
+    getUserAnswers().html(getQuizzAnswers());
+    createDroppables();
+}
+
+/**
+ * Create droppables from users answers placeholders 
+ * with the following configuration:
+ *  1 - Accetps: any element with class .draggable
+ *  2 - Tolerance: touch
+ *  3 - Classes: defaults
+ *  4 - On drop: evaluates draggable rank against droppable rank
+ */
+function createDroppables() {
     // Make Answers droppables
     $('#user-answers p').droppable({
         accepts: '.draggable',
-        tolerance: "touch",        
+        tolerance: "touch",
         classes: {
             "ui-droppable-active": "ui-state-active",
             "ui-droppable-hover": "ui-state-hover"
         },
-        drop: function( event, ui ) {
-            var $currentDroppable = $(this);
-            var evaluation = evaluate($currentDroppable.data('rank'), ui.draggable.data('rank'));
-            if(evaluation == true) {
-                /* Thanks to: https://stackoverflow.com/questions/5581288/how-to-decide-whether-to-accept-or-reject-a-jquery-draggable-into-a-droppable
-                ui.draggable.animate(ui.draggable.data("origPosition"),"slow");*/
-                ui.draggable.animate({
-                    color: 'green'
-                }, 1000);
-                ui.draggable.draggable("option", 'revert', false);
-                ui.draggable.position({
-                    my: 'left',
-                    at: 'right+10',
-                    of: $(this).find('span')
-                });
-                
-                var $ntries = ui.draggable.find('.ntries');
-                var ntriesValue = parseInt($ntries.text().replace(' (', '').replace(')', ''));
-                $ntries.text(" (" + (++ntriesValue) + ")");
-                
-                $(window).on('resize', function(event){
-                    ui.draggable.position({
-                        my: 'left',
-                        at: 'right+10',
-                        of: $currentDroppable.find('span')
-                    });
-                });
-
-                $(this).droppable("disable");
-                ui.draggable.draggable("disable");
-                updateNotification(true);
+        drop: function (event, ui) {
+            if (!quizzStarted) {
+                disableQuizzMenu();
+                addUserRank();
+                quizzStarted = true;
             }
-            else {
-                ui.draggable.animate({
-                    color: 'red'
-                }, 1000);
-                ui.draggable.draggable("option", "revert", true);
-                updateNotification(false);
-                
-                var $ntries = ui.draggable.find('.ntries');
-                var ntriesValue = parseInt($ntries.text().replace(' (', '').replace(')', ''));
-                $ntries.text(" (" + (++ntriesValue) + ")");
-                
-            }
+            onDrop($(this), ui.draggable)
         }
     });
-    loadAnswersFromServer();
 }
 
-function evaluate(rank1, rank2) {
+function onDrop($droppable, $draggable) {
+    var evaluation = evaluateAnswer($droppable.data('rank'), $draggable.data('rank'));
+
+    if (evaluation == true) {
+        updateDraggableOnSuccess($droppable, $draggable);
+    } else {
+        updateDraggableOnFail($droppable, $draggable);
+    }
+
+    updateScore(evaluation);
+    updateAttempts($draggable);
+}
+
+function autoPosition($droppable, $draggable) {
+    $(window).on('resize', function (event) {
+        $draggable.position({
+            my: 'left',
+            at: 'right+10',
+            of: $droppable.find('span')
+        });
+    });
+}
+
+function disableDragAndDrop($droppable, $draggable) {
+    $droppable.droppable("disable");
+    $draggable.draggable("disable");
+}
+
+function updateDraggableOnSuccess($droppable, $draggable) {
+    $draggable
+        .animate({
+            color: 'green'
+        }, 1000)
+        .draggable("option", 'revert', false)
+        .position({
+            my: 'left',
+            at: 'right+10',
+            of: $droppable.find('span')
+        });
+
+    disableDragAndDrop($droppable, $draggable);
+    autoPosition($droppable, $draggable);
+}
+
+function updateDraggableOnFail($droppable, $draggable) {
+    $draggable.animate({
+        color: 'red'
+    }, 1000);
+    $draggable.draggable("option", "revert", true);
+}
+
+function evaluateAnswer(rank1, rank2) {
     return rank1 === rank2;
 }
 
-function updateNotification(doit) {
-    var $ntries = $('#user-results #ntries');
+function updateAttempts($draggable) {
+    var $ntries = $draggable.find('.ntries');
     var ntriesValue = parseInt($ntries.text());
     $ntries.text(++ntriesValue);
-    if(doit) {
-        var $results = $('#user-results #ncorrect');
-        var value = parseInt($results.text());
-        $results.text(++value);
-        
-        var $total = $('#user-results #ntotal');
-        if($results.text() == $total.text() ) {
-           alert('You win!');
-        }
-    }
 }
 
-function getQuizzQuestion() {
-    var $activeOption = $('#top3-options label.active');
-    var $activeCriteria = $('#top3-criteria label.active');
-    var question = createQuestion($activeOption.attr('id'), $activeCriteria.attr('id'));
+function updateScore(doit) {
+    var $attempts = getUserScoreTotalAttempts();
+    var $correct = getUserScoreCorrectAnswers();
+    var $total = getUserScoreTotalAnswers();
+    var $percentage = getUserScorePercentage();
+
+    var attemptsValue = parseInt($attempts.text());
+    var correctValue = parseInt($correct.text());
+    var totalValue = parseInt($total.text());
+
+
+    $attempts.text(++attemptsValue);
+
+    if (doit) {
+        $correct.text(++correctValue);
+        if ($correct.text() == $total.text()) {
+            alert('You win!');
+            quizzStarted = false;
+            removeUserRank();
+            enableQuizzMenu();
+        }
+    }
+
+    $percentage.text(Math.round(100 * correctValue / attemptsValue));
+}
+
+
+
+
+function createQuizzQuestion() {
+    var $option = getActiveOption();
+    var $criteria = getActiveCriteria();
+    var question = createQuestion($option.attr('id'), $criteria.attr('id'));
     return question;
 }
 
@@ -146,44 +315,33 @@ function createQuestion(option, criteria) {
     if (option == 'option-top-countries') {
         if (criteria == 'criteria-embarked') {
             return 'Which country had more people embarked from Afrika?';
-        } 
-        else if (criteria == 'criteria-disembarked') {
+        } else if (criteria == 'criteria-disembarked') {
             return 'Which country had more people disembarked in Europe or in the Americas?';
-        } 
-        else if (criteria == 'criteria-died') {
+        } else if (criteria == 'criteria-died') {
             return 'Which country had more people dying during the Middle Passage?';
         }
-    }
-    else if (option == 'option-top-owners') {
+    } else if (option == 'option-top-owners') {
         if (criteria == 'criteria-embarked') {
             return 'Which ship owners had more people embarked from Afrika?';
-        } 
-        else if (criteria == 'criteria-disembarked') {
+        } else if (criteria == 'criteria-disembarked') {
             return 'Which ship owners had more people disembarked in Europe or in the Americas?';
-        } 
-        else if (criteria == 'criteria-died') {
+        } else if (criteria == 'criteria-died') {
             return 'Which ship owners had more people dead during the Middle Passage?';
         }
-    }
-    else if (option == 'option-top-captains') {
+    } else if (option == 'option-top-captains') {
         if (criteria == 'criteria-embarked') {
             return 'Which ship captains had more people embarked from Afrika?';
-        } 
-        else if (criteria == 'criteria-disembarked') {
+        } else if (criteria == 'criteria-disembarked') {
             return 'Which ship captains had more people disembarked in Europe or in the Americas?';
-        } 
-        else if (criteria == 'criteria-died') {
+        } else if (criteria == 'criteria-died') {
             return 'Which ship captains had more people dead during the Middle Passage?';
         }
-    }
-    else if (option == 'option-top-ships') {
+    } else if (option == 'option-top-ships') {
         if (criteria == 'criteria-embarked') {
             return 'Which ships had more people embarked from Afrika?';
-        } 
-        else if (criteria == 'criteria-disembarked') {
+        } else if (criteria == 'criteria-disembarked') {
             return 'Which ship had more people disembarked in Europe or in the Americas?';
-        } 
-        else if (criteria == 'criteria-died') {
+        } else if (criteria == 'criteria-died') {
             return 'Which ship had more people dead during the Middle Passage?';
         }
     }
@@ -193,39 +351,45 @@ function createQuestion(option, criteria) {
 function getQuizzAnswers(option, criteria) {
     var numberOfTops = parseInt($('#number-of-tops option:selected').text());
     var html = '';
-    for(var i = 1; i <= numberOfTops; i++ ) {
+    for (var i = 1; i <= numberOfTops; i++) {
         html += '<p data-rank="' + i + '">' + i + '.' + ' ';
         html += '<span>' + '</span>'
         html += '</p>';
-        if(i != numberOfTops) {
-           html += "<hr/>";
+        if (i != numberOfTops) {
+            html += "<hr/>";
         }
     }
-    
+
     return html;
 }
 
 
+
+
+
 function loadAnswersFromServer() {
-    var min = parseInt($('#number-of-tops option:selected').val());
-    var type = $('#top3-options label.active').text();
-    var criteria = $('#top3-criteria label.active').text();
+    var min = parseInt(getNumberOfTopsSelector().val());
+    var type = getActiveOption().text();
+    var criteria = getActiveCriteria().text();
     loadQuizzData(min, type, criteria);
 }
 
 function loadQuizzData(min, type, criteria) {
     $.ajax({
         url: 'php/getTops.php',
-        data: {min: min, type: type, criteria: criteria},
-        success: function(data) {
-            answers = configureAnswers(JSON.parse(data), min);
-            putUIElements(answers);
-            enableQuizz();
+        data: {
+            min: min,
+            type: type,
+            criteria: criteria
         },
-        error: function() {
+        success: function (data) {
+            quizzData = JSON.parse(data);
+            putUIElements(configureAnswers(quizzData, min));
+        },
+        error: function () {
             alert("Error loading tops");
         }
-    });    
+    });
 }
 
 
@@ -236,59 +400,76 @@ function showQuizz() {
 function hideQuizz() {
     $('#explore').hide();
 }
-function enableQuizz() {
-    $('#top3-menu').prop("disabled", false);
-}
 
-function disableQuizz() {
-    $('#top3-menu').prop("disabled", true);
-}
-
-function configureAnswers(quizzData, min) {
-    console.log(quizzData);
-    $.each(quizzData, function(key, value) {
+function configureAnswers(data, min) {
+    console.log(data);
+    $.each(data, function (key, value) {
         value['rank'] = (key + 1);
     });
-    
-    quizzData.sort(randomize);
-    quizzData.sort(randomize);
-    
-    return quizzData;
+
+    //quizzData.sort(randomize);    
+    return shuffle(data);
 }
 
-function randomize(a, b) {
-    return .5 - Math.random();           
+// Thanks to: https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array (CoolAJ86 https://stackoverflow.com/users/151312)
+function shuffle(array) {
+    var currentIndex = array.length,
+        temporaryValue, randomIndex;
+
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+
+        // Pick a remaining element...
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+
+        // And swap it with the current element.
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
+
+    return array;
 }
 
 function putUIElements(answers) {
     $('#possible-answers').empty();
-    $.each(answers, function(kez, value) {
-        var button = getButtonFor(value);
+    $.each(answers, function (key, value) {
+        appendButtonFor(value);
     });
 }
 
-function getButtonFor(countryData) {
-    if(countryData.iso2) {
+function appendButtonFor(countryData) {
+    if (countryData.iso2) {
         var countryCode = countryData.iso2.toLowerCase();
         var $flag = $('<img></img')
-            .addClass('flag flag-'+countryCode)
+            .addClass('flag flag-' + countryCode)
             .attr('src', 'img/blank.gif')
             .attr('alt', countryData.name + ' flag')
             .attr('title', countryData.name);
-        
-        var $name = $('<span></span>').text(countryData.name);
-        var $ntries = $('<span class="ntries"></span>').text(' (' + 0 + ')');
-        
+
+        var $name = $('<span></span>')
+            .html('<small>' + countryData.name + '</small>');
+
+        var $flagAndName = $('<span class="pr-1 pl-1"></span>')
+            .append($flag)
+            .append($name);
+
+        var $ntriesHolder = $('<small class="border-left  pl-1  text-muted"></small>');
+        var $ntries = $('<span class="ntries"></span>').text('0');
+        var $x = $('<span>x</span>');
+
+        $ntriesHolder.append($ntries).append($x);
+
         var $answer = $('<div></div>')
             .addClass('draggable')
-            .addClass('m-0 p-0 text-left')
+            .addClass('m-0 pl-2 text-left')
             .data('rank', countryData.rank)
-            .append($flag)
-            .append($name)
-            .append($ntries);
-        
-        $('#possible-answers').append($answer);
-        
+            .append($flagAndName)
+            .append($ntriesHolder);
+
+        getPossibleAnswers().append($answer);
+
         $answer.draggable({
             revert: "invalid"
         });
@@ -296,17 +477,10 @@ function getButtonFor(countryData) {
 }
 
 
+function addUserRank() {
+    
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+function removeUserRank() {
+    
+}
