2650a39c6e2f4b28ebbbd4b7f81eaae70ac91a00
Alexandre Caldeira <zuacaldeira@gmail.com>
2018-12-20 21:15:51 +0100

    Set figure captions and do some styling.

--- js/charts.js
+++ js/charts.js
@@ -1,4 +1,4 @@
-var CHART = null;
+var CHART1 = null;
 var CHART2 = null;
 
 $(document).ready(function () {
@@ -7,50 +7,39 @@ $(document).ready(function () {
 
     handleEvents();
     loadVariables();
-    
+
     // Basic Charting Tool
     initOptions();
-    
+
     // Improvement 1
 });
 
 function initOptions() {
-    $('#select-charts-function')
-        .children()
-        .first()
+    $('#select-charts-function option.active')
         .prop('selected', 'selected')
         .click();
-    $('#select-charts-groupby')
-        .children()
-        .first()
+    $('#select-charts-groupby option.active')
         .prop('selected', 'selected')
         .click();
-    $('#select-charts-type')
-        .children()
-        .first()
+    $('#select-charts-type option.active')
         .prop('selected', 'selected')
         .click();
 }
 
 function initOptions1() {
-    $('#charts-function')
-        .children()
-        .first()
+    $('#charts-function option.active')
         .prop('selected', 'selected')
         .click();
-    $('#function-argument')
-        .children()
-        .first()
+    $('#function-argument  option.active')
         .prop('selected', 'selected')
         .click();
-    $('#function-groupby-variable')
-        .children()
-        .first()
+    $('#function-groupby-variable  option.active')
         .prop('selected', 'selected')
         .click();
-    $('#charts-type')
-        .children()
-        .first()
+    $('#charts-type option.active')
+        .prop('selected', 'selected')
+        .click();
+    $('#orderby option.active')
         .prop('selected', 'selected')
         .click();
 }
@@ -74,14 +63,12 @@ function handleEvents() {
         event.preventDefault();
         updateFormula(getSelectVariable(), getGroupByVariable());
         loadChartData(getSelectVariable(), getGroupByVariable(), getChartTypeValue());
-        //$('#charts-dashboard form button').click();
-        
+
     });
     $('#charts-dashboard-improved select').on('click', 'option', function (event) {
         event.preventDefault();
         updateFormula1();
         loadChartData1();
-        //$('#charts-dashboard-improved form button').click();
     });
 }
 
@@ -126,17 +113,57 @@ function updateFormula(cFunction, cGroupBy) {
     $('#charts-dashboard .formula-function').text(cFunction);
     $('#charts-dashboard .formula-domain-variable').text(cGroupBy);
 }
+
 function updateFormula1() {
     var cFunction = $('#charts-function .active').val();
     var fArgument = $('#function-argument .active').val();
     var groupBy = $('#function-groupby-variable .active').val();
-    
-    $('#charts-dashboard-improved .formula-function').text(cFunction + '(' + fArgument + ')');
-    $('#charts-dashboard-improved .formula-domain-variable').text(groupBy);
+    var orderBy = getOrderBy();
+
+    $('#charts-dashboard-improved .formula-function')
+        .text(cFunction + '(' + fArgument + ')');
+    $('#charts-dashboard-improved .formula-domain-variable')
+        .text(groupBy);
+
+    if (orderBy) {
+        $('#charts-dashboard-improved .formula-orderBy')
+            .text('ORDER BY ' + orderBy.variable + ' ' + orderBy.direction);
+    }
+
+}
+
+function getOrderBy() {
+    var orderBy = $('#orderby .active').val();
+    switch (orderBy) {
+        case 'xASC':
+            return {
+                variable: $('#function-groupby-variable .active').val(),
+                direction: 'ASC'
+            };
+        case 'xDESC':
+            return {
+                variable: $('#function-groupby-variable .active').val(),
+                direction: 'DESC'
+            };
+        case 'fxASC':
+            return {
+                variable: 'value',
+                direction: 'ASC'
+            };
+        case 'fxDESC':
+            return {
+                variable: 'value',
+                direction: 'DESC'
+            };
+    }
+    return {
+        variable: '',
+        direction: ''
+    };
 }
 
 function loadChartData(selectVariable, groupByVariable, chartType) {
-    updateFormula(selectVariable, groupByVariable);
+    //updateFormula(selectVariable, groupByVariable);
     $.ajax({
         url: 'php/getChartData.php',
         data: {
@@ -145,13 +172,10 @@ function loadChartData(selectVariable, groupByVariable, chartType) {
         },
         success: function (response) {
             createChart('#chart', response, chartType);
-            $('#chart-wrapper .chart-caption').html(
-                '<b>Figure 1: ' + 
-                getSelectChartType() + 
-                ' chart of: ' + 
-                getSelectFunction() + 
-                ' ' + getSelectGroupBy() + 
-                '</b>');
+            var code = 
+                '<b class="clearfix">Figure 1: ' + getSelectChartType() +
+                ' chart of <span class="float-right">' + getSelectFunction() + ' | ' + getSelectGroupBy() + '</span></b>'
+            $('#chart-wrapper .chart-caption').html(code);
         },
         error: function () {
             alert('Error while loading chart data...')
@@ -161,14 +185,25 @@ function loadChartData(selectVariable, groupByVariable, chartType) {
 
 function createChart(selector, data, chartType, label) {
     var $chart = $(selector).empty();
-    if (CHART2) {
-        CHART2.destroy();
+    if (selector == '#chart-1') {
+        if (CHART2) {
+            CHART2.destroy();
+        }
+        CHART2 = new Chart($chart, {
+            type: chartType,
+            data: getChartData(data, label),
+            options: getChartOptions(data, chartType)
+        });
+    } else if (selector == '#chart') {
+        if (CHART1) {
+            CHART1.destroy();
+        }
+        CHART1 = new Chart($chart, {
+            type: chartType,
+            data: getChartData(data, label),
+            options: getChartOptions(data, chartType)
+        });
     }
-    CHART2 = new Chart($chart, {
-        type: chartType,
-        data: getChartData(data, label),
-        options: getChartOptions(data, chartType)
-    });
 }
 
 function getChartData(data, label) {
@@ -198,27 +233,29 @@ function getChartOptions(data, cType) {
 
 
 function loadChartData1() {
-    updateFormula1();
     var chartFunction = $('#charts-function .active').val();
     var functionArgument = $('#function-argument .active').val();
     var groupBy = $('#function-groupby-variable .active').val();
+    var orderBy = $('#orderby .active').val();
     var chartType = $('#charts-type').val();
-    
+
     $.ajax({
         url: 'php/getChartData.php',
         data: {
             select: chartFunction + '(' + functionArgument + ')',
-            groupBy: groupBy
+            groupBy: groupBy,
+            orderBy: getOrderBy()
         },
         success: function (response) {
-            createChart('#chart-1', response, chartType, );
-            $('#chart-wrapper-1 .chart-caption').html(
-                '<b>Figure 2: ' + 
-                $('#charts-type').text() + 
-                ' chart of: ' + 
-                $('#charts-function .active').text() + 
-                ' ' + $('#function-groupby-variable .active').text() + 
-                '</b>');
+            createChart('#chart-1', response, chartType);
+            var code = 
+                '<b class="clearfix">Figure 2: ' + $('#charts-type .active').text() + 
+                ' chart of <span class="float-right">' + 
+                chartFunction + '(' + $('#function-argument .active').text() + ')' + 
+                ' | ' + 
+                $('#function-groupby-variable .active').text() + '</span></b>';
+            
+            $('#chart-wrapper-1 .chart-caption').html(code);
         },
         error: function () {
             alert('Error while loading chart data...')
@@ -227,14 +264,18 @@ function loadChartData1() {
 }
 
 function initChartsDashboard1(variables) {
-    $.each(variables, function(key, variable){
-        $('#function-argument').append(createOption(variable));
-        $('#function-groupby-variable').append(createOption(variable));
+    $.each(variables, function (key, variable) {
+        $('#function-argument').append(createOption(variable, 'slaximp'));
+        $('#function-groupby-variable').append(createOption(variable, 'yeardep'));
     });
 }
 
-function createOption(variable) {
+function createOption(variable, activeVar) {
     var $option = $('<option/>');
-    $option.attr('value', variable.name).text(variable.description);
+    $option.attr('value', variable.name).text(variable.name + ' - ' + variable.description);
+
+    if (variable.name == activeVar) {
+        $option.addClass('active');
+    }
     return $option
 }
--- php/getChartData.php
+++ php/getChartData.php
@@ -8,18 +8,36 @@ $result = [];
 $pdo = getPDO();
 $select = getRequestParameter('select');
 $groupBy = getRequestParameter('groupBy');
+$orderBy = getRequestParameter('orderBy');
 
 if($select != null && $groupBy != null) {
-    $result = getChartData($pdo, $select, $groupBy);
+    if($orderBy != null && $orderBy != '' ) {
+        $result = getChartData(
+            $pdo, 
+            $select, 
+            $groupBy, 
+            $orderBy['variable'], 
+            $orderBy['direction']
+        );
+    } 
+    else  {
+        $result = getChartData(
+            $pdo, 
+            $select, 
+            $groupBy, 
+            $groupBy, 
+            'ASC'
+        );
+    }
 }
 
 header('Content-type:application/json;charset=utf-8');
 echo json_encode($result);
 
 
-function getChartData($pdo, $select, $groupBy) {
+function getChartData($pdo, $select, $groupBy, $orderByVariable, $orderByDirection) {
     $query = getQuery([$groupBy, $select.' as value ']) ;
-    $query .= " GROUP BY $groupBy ORDER BY $groupBy DESC";
+    $query .= " GROUP BY $groupBy ORDER BY $orderByVariable $orderByDirection";
     $erg = $pdo->query($query);
     return $erg->fetchAll(PDO::FETCH_NUM);
 }
