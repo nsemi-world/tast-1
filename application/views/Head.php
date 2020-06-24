92ed45f717bebfda504f893a4ac6030b6ab5fcd4
Alexandre Caldeira <zuacaldeira@gmail.com>
2020-06-24 19:03:01 +0200

    Adds first views according to CodeIgniter.

---
+++ application/views/Head.php
@@ -0,0 +1,15 @@
+<head>
+    <title>TastXplorer</title>
+    <meta name="description" content="TastXplorer is an exploration tool of the 
+          Voyages Databse, aiming to give insight to specific aspects of the 
+          trans-atlantic slave trade."/>
+    
+    
+    <!-- STYLESHEETS -->
+    <link rel="stylesheet" href="<?=base_url();?>css/style.css"/>
+    
+    
+    <!-- SCRIPTS -->
+    <script src="js/quizz-tops.js"></script>
+    <script src="js/home.js"></script>
+</head>
--- application/views/welcome/WelcomePage.php
+++ application/views/welcome/WelcomePage.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en" >
+<html xmlns="http://www.w3.org/1999/xhtml" lang="" xml:lang="">
     <?php 
         $this->load->view('Head'); 
         $this->load->view('welcome/WelcomeBody');
