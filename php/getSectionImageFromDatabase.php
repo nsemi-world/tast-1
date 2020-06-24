283ea09af43ce1ffb6d712079aa2e2afffa0cb1d
Alexandre Caldeira <zuacaldeira@gmail.com>
2020-06-24 14:34:41 +0200

    caall ini file with db details.

--- php/getSectionImageFromDatabase.php
+++ php/getSectionImageFromDatabase.php
@@ -5,48 +5,19 @@ require_once('./utils.php');
 header('Content-type:application/json;charset=utf-8');
 
 $name = getRequestParameter('name');
-$width = getRequestParameter('width');
-$height = getRequestParameter('height');
-$force_db = true;
+print_r($name);
 
-if($force_db) {
-    resizeAndStore($name, $width, $height);
-}
-
-else {
-    resizeOnly($name, $width, $height);
-}
-
-
-function resizeOnly($name, $width, $height) {
-    $im = resizeImage($name, $width, $height);
-    echo json_encode(['url' => toDataUrl($im->getImageBlob()), 'stored' => false]);
-}
-
-function resizeAndStore($name, $width, $height) {
-    $pdo = getPDO();
-    $image = getImage($pdo, $name, $width, $height);
-
+if($name) {
+    $image = getImage($name);
     if($image) {
         echo json_encode(['url' => toDataUrl($image->data)]);
     }
-    else {
-        $im = resizeImage($name, $width, $height);
-        echo json_encode(['url' => toDataUrl($im->getImageBlob()), 'stored' => true]);
-        storeImageInDatabase(
-                    $pdo, 
-                    $im->getImageBlob(), 
-                    $im->getImageFormat(), 
-                    $width, 
-                    $height, 
-                    $name
-        );
-    }
-    $pdo = null;
 }
 
-function getImage($pdo, $name, $width, $height) {
-    $query = "SELECT * FROM images WHERE name='$name' AND width=$width AND height=$height";
+
+function getImage($name) {
+    $query = "SELECT * FROM images WHERE `name`='$name'";
+    $pdo = getPDO();
     $erg = $pdo->query($query);
     $result = $erg->fetch(PDO::FETCH_OBJ);
     return $result;
@@ -54,33 +25,6 @@ function getImage($pdo, $name, $width, $height) {
 
 function toDataUrl($blob) {
     return 'data:image/jpg;base64,' . base64_encode($blob);
-}
-
-function resizeImage($name, $width, $height) {
-    $input = realpath("./test/images/original/$name");
-    
-    $im = new Imagick($input);
-    $im->optimizeImageLayers();
-
-    // Compression and quality
-    $im->setImageCompression(Imagick::COMPRESSION_JPEG);
-    $im->setImageCompressionQuality(75);
-    $im->cropThumbnailImage($width, $height);
-    
-    return $im;
-}
-
-function storeImageInDatabase($pdo, $data, $mime, $width, $height, $name) {
-    $query = "INSERT INTO images(name, width, height, data) VALUES (:name, :width, :height, :data)";
-    
-    $stmt = $pdo->prepare($query);
-    $stmt->bindParam(':name', $name);
-    $stmt->bindParam(':width', $width);
-    $stmt->bindParam(':height', $height);
-    $stmt->bindParam(':data', $data, PDO::PARAM_LOB);
-    $result = $stmt->execute();
-    
-    return toDataUrl($data);
 }
 
 ob_end_flush();
--- php/utils.php
+++ php/utils.php
@@ -8,10 +8,10 @@ function getPDO() {
     // p[0]: mysql://b0bd1223927bc6:707fe6cf
     // p[1]: us-cdbr-iron-east-01.cleardb.net/heroku_1bca0db043051c1?reconnect=true
     
-    $host = $ini['host'];
-    $dbname = $ini['dbname'];
-    $username = $ini['user'];
-    $password = $ini['password'];
+    $host       = $ini['host'];
+    $dbname     = $ini['dbname'];
+    $username   = $ini['user'];
+    $password   = $ini['password'];
     
     $pdo = new PDO("mysql:host=".$host."; dbname=".$dbname.'; charset=utf8', $username, $password);
     return $pdo;
