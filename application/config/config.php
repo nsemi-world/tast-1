3ab806898192e04251ff7eb42cfc25991f9451b9
Alexandre Caldeira <zuacaldeira@gmail.com>
2020-06-27 01:31:02 +0200

    Fix configuration data for production environment.

--- application/config/config.php
+++ application/config/config.php
@@ -23,8 +23,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-//$ini = parse_ini_file('../tast.ini');
-$config['base_url'] = 'http://localhost/tast/public';
+$ini = parse_ini_file('../tast.ini');
+$config['base_url'] = $ini['base_url'];
 
 /*
 |--------------------------------------------------------------------------
@@ -381,10 +381,10 @@ $config['encryption_key'] = '';
 $config['sess_driver'] = 'files';
 $config['sess_cookie_name'] = 'ci_session';
 $config['sess_expiration'] = 7200;
-$config['sess_save_path'] = NULL;
+$config['sess_save_path'] = APPPATH.'/session';
 $config['sess_match_ip'] = FALSE;
 $config['sess_time_to_update'] = 300;
-$config['sess_regenerate_destroy'] = FALSE;
+$config['sess_regenerate_destroy'] = TRUE;
 
 /*
 |--------------------------------------------------------------------------
